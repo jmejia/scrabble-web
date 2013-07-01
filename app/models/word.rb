8fd5c36f9a0cad76c7736a4aacdb7e6e95994810
Josh Mejia <joshua.mejia@gmail.com>
2013-07-01 11:35:28 -0600

    Use + instead of {1,} in word format validation.

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,7 +4,7 @@ class Word < ActiveRecord::Base
 
   validates :word,
             :presence => true,
-            :format => {:with => /^[a-zA-Z]{1,}$/}
+            :format => {:with => /^[a-zA-Z]+$/}
 
   def letter_scores
     {"A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4, "G"=>2, "H"=>4, "I"=>1, "J"=>8,
